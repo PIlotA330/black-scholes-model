404988ab3be5dfa89fc36af65e33a74e76be6c48
PIlotA330 <69285986+PIlotA330@users.noreply.github.com>
2025-04-10 16:46:58 -0500

    added proper api call

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/<float:price>/<float:strike>/<float:rate>/<float:time>/<float:vol>")
-def page(price, strike, rate, time, vol):
-    cPrice = calc_bsm_call(price, strike, rate, time, vol)
-    pPrice = calc_bsm_put(price, strike, rate, time, vol)
+# /api/"price,strike,rate,time,vol"
+@app.route("/api/<string:data>")
+def page(data):
+    dataConverted = list(map(float, data.split(",")))
+    cPrice = calc_bsm_call(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
+    pPrice = calc_bsm_put(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
     return f'<p>Call: { cPrice } and Put: { pPrice }</p>'
