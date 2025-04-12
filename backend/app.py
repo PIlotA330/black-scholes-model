3baf1462d75d287ecfcad934dab2ea97b256127a
PIlotA330 <69285986+PIlotA330@users.noreply.github.com>
2025-04-11 19:40:22 -0500

    have the api endpoint return the json data of calcuations

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from model import calc_bsm_call, calc_bsm_put
+from model import calc_bsm_call, calc_bsm_put, calc_delta, calc_gamma, calc_intrinsic, calc_rho, calc_theta, calc_vega
 
 app = Flask(__name__)
 
@@ -13,4 +13,29 @@ def page(data):
     dataConverted = list(map(float, data.split(",")))
     cPrice = calc_bsm_call(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
     pPrice = calc_bsm_put(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
-    return {"callPrice" : cPrice, "putPrice" : pPrice}
+    delta = calc_delta(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
+    gamma = calc_gamma(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
+    vega = calc_vega(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
+    theta = calc_theta(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
+    rho = calc_rho(dataConverted[0], dataConverted[1], dataConverted[2], dataConverted[3], dataConverted[4])
+    intrinsic = calc_intrinsic(dataConverted[0], dataConverted[1])
+
+    return {"price" : {
+        "call" : cPrice, "put" : pPrice
+    }, "greek" : {
+        "delta" : {
+            "call" : delta[0],
+            "put" : delta[1]
+        }, "gamma" : gamma,
+        "vega" : vega,
+        "theta" : {
+            "call" : theta[0],
+            "put" : theta[1]
+        }, "rho" : {
+            "call" : rho[0],
+            "put" : rho[1]
+        }
+    }, "intrinsic": {
+        "call" : intrinsic[0],
+        "put" : intrinsic[1]
+    }}
